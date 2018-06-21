0ba6ac29edbe0f798350a308331a6142ff65a520
yihwan <yihwankim@gmail.com>
2018-06-20 21:18:51 -0700

    refactor file_reader

--- rule-tracker/testing/file_reader_test.rb
+++ rule-tracker/testing/file_reader_test.rb
@@ -1,40 +1,40 @@
-require './config.rb'
+require_relative 'config'
 
 def final_orders_parser(file)
   lines = File.open(file).to_a
   file_name = File.basename(file, ".txt")
 
-  all_rules = []
-
   lines.each_with_index do |line, idx|
     if line.include?("is amended.")
-      if line.include?("CSR")
-        key_line = line
-      else
-        key_line = lines[idx - 1].gsub("\n", " ") + line
+      key_line = line
+      jdx = idx
+
+      until key_line.include?("CSR")
+        jdx -= 1
+        key_line = lines[jdx].gsub("\n", " ") + key_line
       end
 
-      all_rules.push(create_rule(key_line, "amend", "final", file_name))
+      add_to_airtable(create_rule(key_line, "Amend", "Final Order", file_name))
 
     elsif line.include?("is rescinded.")
-      if line.include?("CSR")
-        key_line = line
-      else
-        key_line = lines[idx - 1].gsub("\n", " ") + line
+      key_line = line
+      jdx = idx
+
+      until key_line.include?("CSR")
+        jdx -= 1
+        key_line = lines[jdx].gsub("\n", " ") + key_line
       end
 
-      all_rules.push(create_rule(key_line, "rescind", "final", file_name))
+      add_to_airtable(create_rule(key_line, "Rescind", "Final Order", file_name))
     end
   end
 
-  puts "Final rules: #{all_rules} \n\n"
+  puts "Final rules uploaded to Airtable."
 end
 
 def proposed_orders_parser(file)
   lines = File.open(file).to_a
   file_name = File.basename(file, ".txt")
-
-  all_rules = []
 
   lines.each_with_index do |line, idx|
     if line.include?("PROPOSED AMENDMENT")
@@ -46,7 +46,7 @@ def proposed_orders_parser(file)
         key_line = key_line.gsub("\n", " ") + lines[jdx]
       end
 
-      all_rules.push(create_rule(key_line, "amend", "proposed", file_name))
+      add_to_airtable(create_rule(key_line, "Amend", "Proposed (Formal)", file_name))
     elsif line.include?("PROPOSED RECISSION")
       jdx = idx + 1
       key_line = lines[jdx]
@@ -56,46 +56,46 @@ def proposed_orders_parser(file)
         key_line = key_line.gsub("\n", " ") + lines[jdx]
       end
 
-      all_rules.push(create_rule(key_line, "rescind", "proposed", file_name))
+      add_to_airtable(create_rule(key_line, "Rescind", "Proposed (Formal)", file_name))
     end
   end
 
-  puts "Proposed rules: #{all_rules} \n\n"
+  puts "Proposed rules uploaded to Airtable."
 end
 
 def create_rule(line, action, stage, file_name)
   rule = {}
 
-  if stage == "final"
-    if action == "amend"
-      rule["code"], rule["desc"] = line.match(/^(?<CODE>\d+\s+CSR\s+[-.\d]+)\s+(?<DESCRIPTION>.*?)(?=\s+is amended.$)/).captures
-    elsif action == "rescind"
-      rule["code"], rule["desc"] = line.match(/^(?<CODE>\d+\s+CSR\s+[-.\d]+)\s+(?<DESCRIPTION>.*?)(?=\s+is rescinded.$)/).captures
+  if stage == "Final Order"
+    if action == "Amend"
+      rule[:rule_citation], rule[:rule_description] = line.match(/^(?<CODE>\d+\s+CSR\s+[-.\d]+)\s+(?<DESCRIPTION>.*?)(?=\s+is amended.$)/).captures
+    elsif action == "Rescind"
+      rule[:rule_citation], rule[:rule_description] = line.match(/^(?<CODE>\d+\s+CSR\s+[-.\d]+)\s+(?<DESCRIPTION>.*?)(?=\s+is rescinded.$)/).captures
     end
 
-  elsif stage == "proposed"
-    rule["code"], rule["desc"] = line.match(/^(?<CHAPTER>\d+\s+CSR\s+[-.\d]+)\s+(?<DESCRIPTION>.*?)(?=\. .+$)/).captures
+  elsif stage == "Proposed (Formal)"
+    rule[:rule_citation], rule[:rule_description] = line.match(/^(?<CHAPTER>\d+\s+CSR\s+[-.\d]+)\s+(?<DESCRIPTION>.*?)(?=\. .+$)/).captures
   end
 
-  rule["proposed_action"] = action
-  rule["stage"] = stage
-  rule["source"] = file_name
+  rule[:proposed_action] = action
+  rule[:stage] = stage
+  rule[:source] = file_name
 
-  add_to_airtable(rule)
   return rule
 end
 
 def add_to_airtable(rule)
   airtableRule = AllSoSData.new(
-    "Rule Citation" => rule["code"],
-    "Rule Description" => rule["desc"],
-    "Action Proposed" => rule["proposed_action"],
-    "Stage" => rule["stage"],
-    "Source" => rule["source"]
+    "Rule Citation" => rule[:rule_citation],
+    "Rule Description" => rule[:rule_description],
+    "Proposed Action" => rule[:proposed_action],
+    "Stage" => rule[:stage],
+    "Source" => rule[:source],
+    "date_added" => Time.now
   )
 
   airtableRule.create
 end
 
-# final_orders_parser('./files/orders_test_excerpt.txt')
-proposed_orders_parser('./files/proposed_test_excerpt_short.txt')
+final_orders_parser('./files/orders_test_excerpt.txt')
+# proposed_orders_parser('./files/proposed_test_excerpt_short.txt')
